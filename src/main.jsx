d4406cd1d499af4d5806176dcf63a39291c0af3f
Nitin1112 <nitinboopathy11@gmail.com>
2024-11-03 13:03:55 +0530

    created login

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App.jsx';
 import './assets/css/style.css';
+import Login from './components/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
  {/* Wrap App with BrowserRouter */}
-      <App />
+      {/* <App /> */}
+      <Login/>
   </StrictMode>,
 );
 
